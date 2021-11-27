a73e521cc3d98d8fd5b3541618604045671ea7a1
Tobias Koller <kolleto1@students.zhaw.ch>
2021-11-27 09:49:59 +0100

    New Version Added with Application and Activity Class. Old Version Removed
    New Version Added with Application and Activity Class.
    Old Version Removed

---
+++ Snippets/TrackingApp/app/src/main/java/com/example/trackingapp/TrackingApplication.java
@@ -0,0 +1,128 @@
+package com.example.trackingapp;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+
+public class TrackingApplication  extends Application implements MonitorNotifier {
+    private static final String TAG = "TrackingApplication";
+    public static final Region beaconRegion = new Region("beaconRegion", null, null, null);
+    public static boolean insideRegion = false;
+
+    public void onCreate() {
+        super.onCreate();
+        BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
+
+        // By default Ithe AndroidBeaconLibrary will only find AltBeacons.  f you wish to make it
+        // find a different type of beacon, you must specify the byte layout for that beacon's
+        // advertisement with a line like below.  The example shows how to find a beacon with the
+        // same byte layout as AltBeacon but with a beaconTypeCode of 0xaabb.  To find the proper
+        // layout expression for other beacon types, do a web search for "setBeaconLayout"
+        // including the quotes.
+        //cut out (,d:25-25)
+        beaconManager.getBeaconParsers().clear();
+        //iBeacon Layout = ("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24")));
+
+        beaconManager.setDebug(true);
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Scanning for Beacons");
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        builder.setContentIntent(pendingIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
+                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("My Notification Channel Description");
+            NotificationManager notificationManager = (NotificationManager) getSystemService(
+                    Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channel.getId());
+        }
+        beaconManager.enableForegroundServiceScanning(builder.build(), 456);
+
+        beaconManager.setEnableScheduledScanJobs(false);
+        beaconManager.setBackgroundBetweenScanPeriod(0);
+        beaconManager.setBackgroundScanPeriod(1100);
+
+       beaconManager.addMonitorNotifier(this);
+
+        // If we were monitoring *different* regions on the last run of this app, they will be
+        // remembered.  In this case we need to disable them here
+        for (Region region: beaconManager.getMonitoredRegions()) {
+            beaconManager.stopMonitoring(region);
+        }
+
+        beaconManager.startMonitoring(beaconRegion);
+
+    }
+
+
+    @Override
+    public void didEnterRegion(Region region) {
+        Log.d(TAG, "did enter region.");
+        insideRegion = true;
+        // Send a notification to the user whenever a Beacon
+        // matching a Region (defined above) are first seen.
+        Log.d(TAG, "Sending notification.");
+        sendNotification();
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        insideRegion = false;
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int state, Region region) {
+    }
+
+
+
+    private void sendNotification() {
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Tracking App Notifications",
+                    "Tracking App Notifications", NotificationManager.IMPORTANCE_HIGH);
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            notificationManager.createNotificationChannel(channel);
+            builder = new Notification.Builder(this, channel.getId());
+        }
+        else {
+            builder = new Notification.Builder(this);
+            builder.setPriority(Notification.PRIORITY_HIGH);
+        }
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntent(new Intent(this, MainActivity.class));
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        builder.setContentTitle("I detect a beacon");
+        builder.setContentText("Tap here to see details in the tracking app");
+        builder.setContentIntent(resultPendingIntent);
+        notificationManager.notify(1, builder.build());
+    }
+}
